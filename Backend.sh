bf9f25e28b695969a304cfe011a15a215cf84e49
RunKiran <kiranmadasu790@gmail.com>
2024-04-24 20:08:40 +0530

    some-message

--- Backend.sh
+++ Backend.sh
@@ -51,7 +51,7 @@ else
 
 fi
 
-mkdir /app &>>$LOGFILE
+mkdir -p /app &>>$LOGFILE
 VALIDATE $? "creating app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
